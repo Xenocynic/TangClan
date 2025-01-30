ce150c5c4eb6a6c364134e370ae381452f478252
jpagcal <jpagcal_@outlook.com>
2025-01-29 19:25:34 -0700

    add hex!

---
+++ code/TangClan/app/src/main/java/com/example/tangclan/Hexagon.java
@@ -0,0 +1,10 @@
+package com.example.tangclan;
+
+public class Hexagon extends Shape {
+    public int edgeLength;
+    public Hexagon(int x, int y, int edgeLength) {
+        this.x = x;
+        this.y = y;
+        this.edgeLength = edgeLength;
+    }
+}
