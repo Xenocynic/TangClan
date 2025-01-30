2066dba6fb7a8d1dca98f28f48409ce7d93c9fac
AlissaPepino <alissapepino@gmail.com>
2025-01-29 18:39:35 -0700

    Added Setters and Getters in Shape Class

--- code/TangClan/app/src/main/java/com/example/tangclan/Shape.java
+++ code/TangClan/app/src/main/java/com/example/tangclan/Shape.java
@@ -3,4 +3,20 @@ package com.example.tangclan;
 public abstract class Shape {
     int x;
     int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
