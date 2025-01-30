14101f226604e942dfb5888b1c4a690dc0b63620
Hiteshwari Nanda <hiteshwarinanda@Hiteshwaris-MacBook-Air-2.local>
2025-01-29 21:27:18 -0700

    Implement chnages

--- code/TangClan/app/src/main/java/com/example/tangclan/Shape.java
+++ code/TangClan/app/src/main/java/com/example/tangclan/Shape.java
@@ -3,7 +3,7 @@ package com.example.tangclan;
 public abstract class Shape {
     int x;
     int y;
-    String color = "green";
+    String color = "red";
 
     // Setters and Getters
     public int getX() {
