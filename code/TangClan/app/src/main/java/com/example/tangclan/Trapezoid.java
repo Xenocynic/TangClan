4cf5beeaaf76b485be40c5bce9a0ca0658c6e486
ShaianSh <166669563+ShaianSh@users.noreply.github.com>
2025-01-29 20:02:17 -0700

    Test2

---
+++ code/TangClan/app/src/main/java/com/example/tangclan/Trapezoid.java
@@ -0,0 +1,8 @@
+public class Trapezoid extends Shape {
+    public int edgeLength;
+    public Trapezoid(int x, int y, int edgeLength) {
+        this.x = x;
+        this.y = y;
+        this.edgeLength = edgeLength;
+    }
+}
