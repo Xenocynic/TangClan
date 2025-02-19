8e6186ce597154b557e462df8ae192b1ebf66a8a
ShaianSh <166669563+ShaianSh@users.noreply.github.com>
2025-02-19 16:09:38 -0700

    Profile Class

--- code/TangClan/app/src/main/java/com/example/tangclan/Trapezoid.java
+++
@@ -1,8 +0,0 @@
-public class Trapezoid extends Shape {
-    public int edgeLength;
-    public Trapezoid(int x, int y, int edgeLength) {
-        this.x = x;
-        this.y = y;
-        this.edgeLength = edgeLength;
-    }
-}
